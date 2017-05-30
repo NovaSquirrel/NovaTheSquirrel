d07f22a482f94086a5b235231b6196fdc3d8610e
NovaSquirrel <bushytail.squirrel@gmail.com>
2017-05-30 02:44:10 -0400

    Updated copyrights to 2017

--- src/leveldecode.s
+++ src/leveldecode.s
@@ -1,5 +1,5 @@
 ; Princess Engine
-; Copyright (C) 2014-2016 NovaSquirrel
+; Copyright (C) 2014-2017 NovaSquirrel
 ;
 ; This program is free software: you can redistribute it and/or
 ; modify it under the terms of the GNU General Public License as
