8a87c3616b41893e8a0a37cfea40c3b76270b560
NovaSquirrel <bushytail.squirrel@gmail.com>
2019-03-23 22:17:51 -0400

    Updated title screen (again)

--- tools/title/converttitle.bat
+++ tools/title/converttitle.bat
@@ -1,4 +1,4 @@
-py -2 savtool.py --palette=300f2a12300f2a26300f122232000000 title2.png title.sav
+py -2 savtool.py --palette=300f2a12300f2a26300f122232000000 title3.png title.sav
 py -2 savtool.py title.sav title.chr
 py -2 savtool.py title.sav title.nam
 py -2 pb53.py -i title.chr -o title.pb53 --raw
